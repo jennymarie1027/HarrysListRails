b16a4281cd64bf8efc5a61dbf446342ed2a822bd
McGrath <jennymarie1027@gmail.com>
2025-01-16 11:48:50 -0700

    edit and delete user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,21 +19,49 @@ class UsersController < ApplicationController
         render json: @user
     end
 
-    def new
+    def new # what is the point of this action 
+        @user = User.new
     end
 
     def create
+        # filter params so only allowed attributes are passed
+        user_params
+        # create new object
+        @user = User.new(params[:user])
+        # save object
+        if @user.save
+        # if succeeeds redirect to index
+            redirect_to users_path
+        # if fails render new
+        else
+            render :new
+        end
     end
 
     def edit
+        @user = User.find(params[:id])
     end
 
     def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to users_path(@user)
+        else
+            render :edit
     end
 
     def delete
+        @user = User.find(params[:id])
     end
 
     def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect :users_path
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
 end
