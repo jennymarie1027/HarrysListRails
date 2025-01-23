f175699fdf051c0f21fb8e3767699e258ccb81b8
McGrath <jennymarie1027@gmail.com>
2025-01-23 11:55:39 -0600

    outline first test

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :user do
-        sequence(:email) { |n| 'rspec_test_user' + n.to_s + '@example.com' }
+        sequence(:email) { |n| 'rspec_test_user' + n.to_s + '@test.com' }
         password { 'password' }
     end
 end
