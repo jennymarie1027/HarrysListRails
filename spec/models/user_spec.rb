528eec7ce56dac13fdd4bb9d2861f6700120dd27
McGrath <jennymarie1027@gmail.com>
2025-01-31 16:25:22 -0700

    tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe User, type: :model do
     it "creates a user" do
       user = FactoryBot.create(:user)
       expect(user).to be_valid
-      expect(:status).to eq(201)
+      expect(user.email).to include("rspec_test_user")
+    end
+
+    it "creates an invalid user" do
+      invalid_user = FactoryBot.create(:invalid_user)
+      expect(invalid_user).to be_invalid
     end
   end
 end
+
+# Rspec.describe InvalidUser, type: :model do
+ 
+# end
