8155da4bcca99c1e0d567a54c346370e786f5b06
McGrath <jennymarie1027@gmail.com>
2025-02-03 16:41:19 -0700

    start outline of tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,8 +3,4 @@ FactoryBot.define do
         sequence(:email) { |n| 'rspec_test_user' + n.to_s + '@test.com' }
         password { 'password' }
     end
-
-    factory :invalid_user do
-        sequence(:invalid_user) { |n| 'rspec_test_user' + n.to_s + '@test.com' }
-    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "POST" do
+  let(:user) { FactoryBot.build(:user) }
+
+  describe "validations" do
     it "creates a user" do
-      user = FactoryBot.create(:user)
       expect(user).to be_valid
       expect(user.email).to include("rspec_test_user")
     end
 
-    it "creates an invalid user" do
-      invalid_user = FactoryBot.create(:invalid_user)
-      expect(invalid_user).to be_invalid
+    it "is not valid without a email" do
+      user.email = nil
+      expect(user).not_to be_valid
     end
+
+    it "is not valid without a password" do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+
+    it "it not valid with duplicate email" do
+      existing_user = FactoryBot.create(:user)
+      duplicate_user = FactoryBot.build(:user, email: existing_user.email)
+      expect(duplicate_user).not_to be_valid
+    end
+
+    # it "is not valid with a short password" do
+    #   user.password = "123"
+    #   expect(user).not_to be_valid
+    # end
+
+    # it 'is not valid with an incorrectly formatted email' do
+    #   invalid_emails = ['invalid_email', 'missing@domain', 'no_at_sign.com']
+    #   invalid_emails.each do |invalid_email|
+    #     user.email = invalid_email
+    #     expect(user).not_to be_valid
+    #   end
+    # end
+
   end
 end
 
