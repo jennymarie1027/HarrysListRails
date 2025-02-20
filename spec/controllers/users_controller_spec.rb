0b3e899f9517a65da0f0894ba1d5ea2d13cfde9e
McGrath <jennymarie1027@gmail.com>
2025-02-20 16:23:15 -0700

    user controller spec index

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe UsersController do
+  describe "Get #index" do
+  before do
+    # Create multiple users
+    FactoryBot.create_list(:user, 12)
+  end
+
+    it "returns a successful response" do
+      get :index
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body).length).to eq(12)
+    end
+  end
+end
