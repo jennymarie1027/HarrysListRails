e40689521e47d8c21c4aefb5331b26b5564a5473
McGrath <jennymarie1027@gmail.com>
2025-03-18 10:01:07 -0600

    post scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
     # Add a belongs_to association to the Post model
     belongs_to :user, optional: false  # Add this line to associate a post with a user
+
+    scope: :alphabetical, -> { order(:title) }
+    scope: :user_faves, -> (user) { where(user_id: user.id, is_fave: true) }
+    scope: :cheap, -> { where("price < 10") }
 end
