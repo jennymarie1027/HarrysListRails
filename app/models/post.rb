875a0507d3dd4a3ebbaa102110933952405d58a2
McGrath <jennymarie1027@gmail.com>
2025-03-18 10:23:15 -0600

    most post scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,7 @@ class Post < ApplicationRecord
     scope: :alphabetical, -> { order(:title) }
     scope: :user_faves, -> (user) { where(user_id: user.id, is_fave: true) }
     scope: :cheap, -> { where("price < 10") }
+    scope: :expensive, -> { where("price > 100") }
+    scope: :search_title_by_keyword, -> (keyword) { where("title LIKE ?", "%#{keyword}%") }
+    scope: :search_desc_by_keyword, -> (keyword) { where("description LIKE ?", "%#{keyword}%") }
 end
