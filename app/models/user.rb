6fad5c1ee78b9f9dba3ec52d6a9d147679f1acaf
McGrath <jennymarie1027@gmail.com>
2025-03-18 09:58:46 -0600

    user model action and scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   
     # Ensure email is present and unique
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+    scope: :alphabetical, -> { order(:first_name, :last_name) }
+    scope: :faves, -> { where(is_fave: true)}
+    scope: :find_by_email, -> (email) { where(email: email).first }
+    scope: :find_by_credentials, -> (email, password) { user = User.find_by_email(email); user && user.authenticate(password) ? user : nil }
+    scope: :recently_created, -> { where("created_at > ?", 1.week.ago) }
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+
 end
