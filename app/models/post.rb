98a3aad891acba179fd23e5b38fe9f428ed91538
McGrath <jennymarie1027@gmail.com>
2025-03-20 11:48:13 -0600

    add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@ class Post < ApplicationRecord
     # Add a belongs_to association to the Post model
     belongs_to :user, optional: false  # Add this line to associate a post with a user
 
+    # Add validations to the Post model
+    validates :user_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validates :title, presence: true, length: { minimum: 2 }
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :category, presence: true # , inclusion: { in: %w(ForSale Wanted) }
+    validates :description, presence: true, length: { minimum: 1 }
+
+
     scope :user_faves, -> (user) { where(user_id: user.id, is_fave: true) }
     scope :cheap, -> { where("price < 10") }
     scope :expensive, -> { where("price > 100") }
